e6db6c985779bc7c9a3834f222b1df2ef4889311
andreabdel27 <andreabdel27@gmail.com>
2017-07-10 13:58:04 -0400

    qr generator activity with action bar save icon to store bitmap image in x location. still missing encoding data

--- app/src/main/java/com/example/root/monerotest/QRGenerator/QRGeneratorActivity.java
+++ app/src/main/java/com/example/root/monerotest/QRGenerator/QRGeneratorActivity.java
@@ -1,9 +1,15 @@
 package com.example.root.monerotest.QRGenerator;
 
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.example.root.monerotest.R;
 
@@ -23,6 +29,12 @@ public class QRGeneratorActivity extends AppCompatActivity{
         setContentView(R.layout.qr_generator_activity);
 
 
+
+        if(getSupportActionBar() != null){
+            getSupportActionBar().setBackgroundDrawable(new ColorDrawable(ContextCompat.getColor(this, R.color.colorPrimary)));
+            getSupportActionBar().setTitle(R.string.qr_bitmap);
+        }
+
         getFragmentManager().beginTransaction().
                 replace(R.id.fragment_container,
                         QRGeneratorFragment.newInstance(),
@@ -30,4 +42,21 @@ public class QRGeneratorActivity extends AppCompatActivity{
                 .commit();
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(item.getItemId() == R.id.action_save_to_storage){
+            //TODO: handle storage of the QR bitmap.
+
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.qr_generator_menu, menu);
+        return true;
+    }
 }
