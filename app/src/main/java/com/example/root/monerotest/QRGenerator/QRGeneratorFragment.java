e8a83e07f4c7baaef2ba923ec4afe65225762831
andreabdel27 <andreabdel27@gmail.com>
2017-07-10 12:38:24 -0400

    QR image showing up without any encoded data. still missing a way to store the bitmap so it can be send to anyone

--- app/src/main/java/com/example/root/monerotest/QRGenerator/QRGeneratorFragment.java
+++ app/src/main/java/com/example/root/monerotest/QRGenerator/QRGeneratorFragment.java
@@ -2,13 +2,18 @@ package com.example.root.monerotest.QRGenerator;
 
 
 import android.app.Fragment;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.root.monerotest.R;
+import com.google.zxing.qrcode.encoder.QRCode.*;
+
+import net.glxn.qrgen.android.QRCode;
 
 /**
  * Andrea Abdelnour
@@ -29,8 +34,19 @@ public class QRGeneratorFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.qr_generator_fragment, container, false);
-
-
         return v;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        View views = getView();
+        if(views == null)
+            return;
+
+
+        Bitmap QRImage = QRCode.from("asdasdasd").bitmap();
+
+        ((ImageView) views.findViewById(R.id.imageview_qr)).setImageBitmap(QRImage);
     }
 }
